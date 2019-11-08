2d6c08dc48852b03d143dce9b836422d0bdd9601
Lara Bjork <lara.m.bjork@gmail.com>
2019-11-08 10:52:52 -0800

    add jQuery to allow user to see what size they have selected in order confirmation section

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-function Pizza (size, toppings) {
-  this.size = size,
-  this.toppings = toppings,
-  this.price = 0
-};
-
-Pizza.prototype.getPrice = function () {
-  // this.toppings.forEach(function (topping)
-  if (this.size === small) {
-    this.price += 10;
-  } else if (this.size === medium) {
-    this.price += 15;
-  } else {
-    this.price += 20;
-  }
-  console.log(Pizza.getPrice)
-};
+// function Pizza (size, toppings) {
+//   this.size = size,
+//   this.toppings = toppings,
+//   this.price = 0
+// };
+//
+// Pizza.prototype.getPrice = function () {
+//   // this.toppings.forEach(function (topping)
+//   if (this.size === small) {
+//     this.price += 10;
+//   } else if (this.size === medium) {
+//     this.price += 15;
+//   } else {
+//     this.price += 20;
+//   }
+//   console.log(Pizza.getPrice)
+// };
 
 
 
@@ -28,17 +28,19 @@ Pizza.prototype.getPrice = function () {
 // }
 
 $(document).ready(function (){
-  $("form#orderForm").submit(event){
+  $("form#order-form").submit(function(event) {
   event.preventDefault();
   let sizeInput = $("input:radio[name=size]:checked").val();
+  $(".ordered-size").text(sizeInput);
+
   // let toppingsInputs = $(#toppings: checked)://this id doesn't exist yet;
   // let toppingsArray = [];
   // toppingsInputs.forEach(function(toppingInput){
   //   toppingsArray.push(toppingInput.val());
   // });
+//
+// let pizzaOrder = new Pizza(sizeInput, toppingsArray);
+// let price = pizzaOrder.getPrice();
 
-let pizzaOrder = new Pizza(sizeInput, toppingsArray);
-let price = pizzaOrder.getPrice();
-
-}
+});
 });
