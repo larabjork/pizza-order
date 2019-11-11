64009fcac12064eaf3a5fc869c17b18035048819
Lara Bjork <lara.m.bjork@gmail.com>
2019-11-10 20:07:35 -0800

    add ability to display all order information to user, including fixes to toppings list so that words and commas are spaced correctly

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Pizza (size, toppings) {
 };
 
 Pizza.prototype.getPrice = function () {
-this.price = this.price + this.toppings.length
+  this.price = this.price + this.toppings.length
   if (this.size === "Small") {
     this.price += 10;
   } else if (this.size === "Medium") {
@@ -26,12 +26,17 @@ $(document).ready(function (){
     let toppingsArray = [];
     for (var i = 0; i < toppingsInputs.length; i +=1) {toppingsArray.push(toppingsInputs[i].value);
    }
+   console.log(toppingsArray)
 
 
     let myPizza =new Pizza(sizeInput,toppingsArray);
     myPizza.getPrice();
 
+    let toppingsList = toppingsArray.toString();
+    toppingsList = toppingsList.replace("greenPeppers", "green peppers").replace("mozzarella", "fresh mozzarella").replace("olives", "black olives").replace("basil", "fresh basil").replace(/,/g, ", ");
+
     $(".ordered-size").html(sizeInput);
+    $(".ordered-toppings").html(toppingsList);
     $(".ordered-price").html(myPizza.price);
     console.log(myPizza)
   });
